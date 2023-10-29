f1726a38c65ac82cdf8c44cc72c88ba84468bd88
HaimOzer <sandromoya@icloud.com>
2023-10-29 19:30:24 +0200

    add brain-gcd.js

---
+++ bin/games/brain-gcd.js
@@ -0,0 +1,43 @@
+import readlineSync from 'readline-sync';
+import greetAndGetName from '../../src/cli.js';
+
+// MAIN BEGIN
+function gcd(a, b) {
+  while (b !== 0) {
+      let temp = b;
+      b = a % b;
+      a = temp;
+  }
+  return a;
+}
+// MAIN END
+
+function playGCD(name) {
+  console.log('Find the greatest common divisor of given numbers.');
+
+  let correctAnswersCount = 0;
+  const roundsToWin = 3;
+
+  while (correctAnswersCount < roundsToWin) {
+    const num1 = Math.floor(Math.random() * 10) + 1;
+    const num2 = Math.floor(Math.random() * 10) + 1;
+    console.log(`Question: ${num1} ${num2}`);
+    let expectedAnswer = gcd(num1, num2)
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (parseInt(userAnswer, 10) === expectedAnswer) {
+      console.log('Correct!');
+      correctAnswersCount += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      break;
+    }
+  }
+
+  if (correctAnswersCount === roundsToWin) {
+    console.log(`Congratulations, ${name}!`);
+  }
+}
+const name = greetAndGetName();
+playGCD(name);
