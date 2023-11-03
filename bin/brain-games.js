d16ba35e2f91dd7903e1dfbdf0c25f59e0e9eb50
HaimOzer <sandromoya@icloud.com>
2023-11-03 22:23:11 +0200

    restore brain-game

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,16 +1,5 @@
 #!/usr/bin/env node
 
 import greetAndGetName from '../src/cli.js';
-import playEvenGame from './games/brain-even.js';
-import playCalcGame from './games/brain-calc.js';
-import playGCD from './games/brain-gcd.js';
-import playProgressionGame from './games/brain-progression.js';
-import playBrainPrime from './games/brain-prime.js';
 
-const name = greetAndGetName();
-
-playEvenGame(name);
-playCalcGame(name);
-playGCD(name);
-playProgressionGame(name);
-playBrainPrime(name);
+greetAndGetName();
