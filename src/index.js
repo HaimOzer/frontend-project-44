37accd36a070652a1a207d7de0bda3cc5018ec09
HaimOzer <sandromoya@icloud.com>
2023-11-07 21:07:08 +0200

    upgrade index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function buildGameLogic(gameDescription, createGameContent) {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  console.log(gamesDescription);
+  console.log(gameDescription);
 
   const roundsToWin = 3;
 
