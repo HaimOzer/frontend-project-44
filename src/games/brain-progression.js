72925c09afca44cbd1a0ba671173182c291a9c01
HaimOzer <sandromoya@icloud.com>
2023-11-08 19:55:37 +0200

    add brain-progression to src/games

---
+++ src/games/brain-progression.js
@@ -0,0 +1,30 @@
+import buildGameLogic from '../index.js';
+import getRandomNumber from '../getRandomNumber.js';
+
+const gameDescription = 'What number is missing in the progression?';
+
+const progressionLength = 10;
+
+function createProgression(beginProgression, interval) {
+  let result = [];
+  for (let i = 1; i <= progressionLength; i += 1) {
+    result.push(beginProgression + interval * i);
+  }
+  return result;
+}
+
+function createGameContent() {
+  const beginProgression = getRandomNumber(1, 30);
+  const interval = getRandomNumber(2, 9);
+  const progressionLine = createProgression(beginProgression, interval);
+  const hiddenElementOfLine = getRandomNumber(0, progressionLine.length -1);
+  const correctAnswer = String(progressionLine[hiddenElementOfLine]);
+  const question = progressionLine.join(' ').replace(correctAnswer, '..');
+  return [question, correctAnswer];
+}
+
+function playProgressionGame() {
+  buildGameLogic(gameDescription, createGameContent);
+}
+
+export default playProgressionGame;
