ce82aaeaabe5ecc2b5eab174e6ffbd121196c8b1
HaimOzer <sandromoya@icloud.com>
2023-11-09 19:38:46 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import readlineSync from 'readline-sync'; // user input function
 
+const roundsToWin = 3;
+
 function buildGameLogic(gameDescription, createGameContent) {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(gameDescription);
-
-  const roundsToWin = 3;
 
   for (let round = 1; round <= roundsToWin; round += 1) {
     const [question, correctAnswer] = createGameContent();
