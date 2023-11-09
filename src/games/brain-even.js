a5df8f1efd91780558bdbfbc1f001bf45e18fe4f
HaimOzer <sandromoya@icloud.com>
2023-11-09 19:43:02 +0200

    update brain-even

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -2,6 +2,7 @@ import buildGameLogic from '../index.js';
 import getRandomNumber from '../getRandomNumber.js';
 
 const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+
 const isEven = (number) => number % 2 === 0;
 
 function createGameContent() {
